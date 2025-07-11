e989b772612d23f3b37fcee57117d8e2cc47760a
mtraYuriCoronado <mtra.yuricoronado@gmail.com>
2025-07-11 10:25:52 -0600

    Modificación del controlador para validad la categoria al agregar productos

--- controllers/producto.controller.js
+++ controllers/producto.controller.js
@@ -12,6 +12,7 @@ exports.obtenerTodos = async (req, res) => {
       }
     });
     res.json(productos);
+
   } catch (error) {
     res.status(500).json({ error: 'Error al obtener productos' });
   }
@@ -20,8 +21,17 @@ exports.obtenerTodos = async (req, res) => {
 // POST
 exports.crear = async (req, res) => {
   try {
+    const { id_categoria } = req.body;
+
+    // Validar que la categoría exista
+    const categoria = await Categoria.findByPk(id_categoria);
+    if (!categoria) {
+      return res.status(400).json({ error: 'La categoría no existe' });
+    }
+
     const producto = await Producto.create(req.body);
     res.status(201).json(producto);
+
   } catch (error) {
     res.status(500).json({ error: 'Error al crear producto' });
   }
@@ -30,16 +40,25 @@ exports.crear = async (req, res) => {
 // PUT
 exports.actualizar = async (req, res) => {
   try {
+    
     const { id } = req.params;
     const producto = await Producto.findByPk(id);
-    console.log(id)
 
     if (!producto) {
       return res.status(404).json({ error: 'Producto no encontrado' });
     }
 
+     // Si se está actualizando la categoría, validarla
+    if (req.body.id_categoria) {
+      const categoria = await Categoria.findByPk(req.body.id_categoria);
+      if (!categoria) {
+        return res.status(400).json({ error: 'La categoría no existe' });
+      }
+    }
+
     await producto.update(req.body);
     res.json({ mensaje: 'Producto actualizado correctamente', producto });
+    
   } catch (error) {
     res.status(500).json({ error: 'Error al actualizar el producto' });
   }
